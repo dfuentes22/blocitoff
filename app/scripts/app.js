939fa699d86d2bb8bc33b7a1323706ac1d72d6dc
Dan <dvf5104@gmail.com>
2015-11-16 20:21:11 -0500

    Set up firebase

---
+++ app/scripts/app.js
@@ -0,0 +1,7 @@
+var blocitoff = angular.module('blocitoff', ['firebase']);
+
+blocitoff.controller("Task.controller", ["$scope", "$firebaseArray",
+  function($scope, $firebaseArray) {
+    // ...
+  }
+]);
