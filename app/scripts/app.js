ca7097080bf14a7b6ddffc17a89944b8f487945b
Dan <dvf5104@gmail.com>
2015-11-22 20:42:00 -0500

    Configured Firebase, need to generate and save tasks

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,9 @@
-var blocitoff = angular.module('blocitoff', ['firebase']);
+var blocitoff = angular.module('blocitoff', ['firebase', 'ui-router']);
 
-blocitoff.controller("Task.controller", ["$scope", "$firebaseArray",
-  function($scope, $firebaseArray) {
-    // ...
-  }
+blocitoff.controller("Task.controller", ["$scope", "$firebaseArray", function($scope, $firebaseArray) {
+    var ref = new Firebase("https://blocitoff5104.firebaseio.com");
+    // download the data into a local object
+    $scope.data = $firebaseArray(ref);
+
+    }
 ]);
